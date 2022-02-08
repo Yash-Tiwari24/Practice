c3a2ddc87386d5ecf4c3da79c3f8be28e1f0fdd8
Yash-Tiwari24 <yash.t@thinkbridge.in>
2022-02-08 16:17:11 +0530

    Added Product Class

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Practice.Models
+{
+    public class Product
+    {
+        public int pid { get; set; }
+    }
+}

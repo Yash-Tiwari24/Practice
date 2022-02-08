b1cf85d9d2fdc02c685689f3ef0fa273756a8979
Yash-Tiwari24 <yash.t@thinkbridge.in>
2022-02-08 12:52:19 +0530

    Added Employee Class

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Practice.Models
+{
+    public class Employee
+    {
+        public int Empid { get; set; }
+        public string name { get; set; }
+    }
+}
